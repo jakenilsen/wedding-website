9bc52e1a754fc08cbfa41c9ac72443d8c74bf4ac
DESKTOP-A310VE7\Jacob <jacobmnilsen@gmail.com>
2025-03-29 15:17:18 +0100

    Fikset litt for våre navn

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Suji og Jacobs Bryllup",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Aug 02, 2025 13:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Aug 03, 2025 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Greåker Frikirke, Klevaveien 2, 1719 Greåker',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "Vi kan ikke vente med å treffe dere på den store dagen vår! For spørsmål kontakt Jacob på +47 90276455."
         }
     });
 
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbw8BcOAtuCwVwGXedkzo1hvdX__8dGJJHwFh_1l3yDkWeDju4RQgYDLgvyj4VmYjt0Dpw/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -241,15 +241,21 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: location,
+        zoom: 12,
+        center: {lat: 59.236585575686725, lng: 10.986828633212378},
         scrollwheel: false
     });
 
-    var marker = new google.maps.Marker({
-        position: location,
+    var markerVielse = new google.maps.Marker({
+        position: {lat: 59.269984499037804, lng: 11.03005574396837},
+        label: 'Vielse',
+        map: map
+    });
+
+    var markerselskap = new google.maps.Marker({
+        position: {lat: 59.202949451388456, lng: 10.952147043964889},
+        label: 'Selskap',
         map: map
     });
 }
